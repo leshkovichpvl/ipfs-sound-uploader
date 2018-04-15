5a56b6f33315973b13d841b24dc16f59d48c959a
leshkovichpvl <leshkovichpvl@gmail.com>
2018-04-15 13:35:08 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 
 import * as React from 'react';
+import * as IpfsAPI from 'ipfs-api';
+
+
+
+const f = IpfsAPI({host: 'localhost', port: '5002'});
+
+f.id(function (err, identity) {
+    if (err) {
+        throw err
+    }
+    console.log(identity)
+})
 
 
 
