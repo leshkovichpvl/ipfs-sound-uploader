b4dfc2fe52023f45a745f3de046e110a85f5fd25
leshkovichpvl <leshkovichpvl@gmail.com>
2018-04-15 13:56:59 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import * as IpfsAPI from 'ipfs-api';
 
 
 
-const f = IpfsAPI({host: 'localhost', port: '5002'});
+const f = IpfsAPI({host: 'http://app-8d7b3d89-ba18-47b7-934e-29609fe74cc1.cleverapps.io/', port: '5002'});
 
 f.id(function (err, identity) {
     if (err) {
