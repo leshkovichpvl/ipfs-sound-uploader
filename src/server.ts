5a964a3156aba8ade244caeda833d88631b3d372
leshkovichpvl <leshkovichpvl@gmail.com>
2018-04-15 13:43:14 +0300

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-const IPFS = require('ipfs')
+const http = require('http');
 
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
+});
 
-const repoPath = 'ipfs-' + Math.random()
-const ipfs = new IPFS({ repo: repoPath })
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env.PORT, '0.0.0.0');
